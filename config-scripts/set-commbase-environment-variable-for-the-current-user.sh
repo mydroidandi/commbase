34d488b73071bbf508bee6a70e449f1fd91bf427
Esteban Herrera <stv.herrera@gmail.com>
2022-11-24 15:20:44 -0600

    Refactor set commbase environment variable config script

---
+++ config-scripts/set-commbase-environment-variable-for-the-current-user.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+# set-commbase-environment-variable-for-the-current-user.sh
+
+# Set environment variable permanently, making each user to have a
+# separate setting
+if [[ ! -d "$COMMBASE" ]]; then 
+    echo "" >> ~/.bashrc
+    echo "# The Commbase directory for the Bash shell" >> ~/.bashrc
+    echo 'export COMMBASE="$HOME/Developer/mydroidandi"' >> ~/.bashrc
+    echo "" >> ~/.zshrc
+    echo "# The Commbase directory for the Z shell" >> ~/.zshrc
+    echo 'export COMMBASE="$HOME/Developer/mydroidandi"' >> ~/.zshrc
+fi
+
