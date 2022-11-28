c346fb236bff5f0252e9ccfc075540a930343bb5
Esteban Herrera <stv.herrera@gmail.com>
2022-11-28 17:56:10 -0600

    Update configuration constant and setup .bak files for devs

--- config-scripts/set-commbase-root-directory.sh
+++ config-scripts/set-commbase-root-directory.sh
@@ -3,13 +3,13 @@
 
 # Set environment variable permanently, making each user to have a
 # separate setting
-if [[ ! -d "$COMMBASE" ]]; then 
+if [[ ! -d "$COMMBASE_ROOT_DIR" ]]; then 
     echo "" >> ~/.bashrc
     echo "# The Commbase directory for the Bash shell" >> ~/.bashrc
-    echo 'export COMMBASE="$HOME/Developer/mydroidandi"' >> ~/.bashrc
+    echo 'export COMMBASE_ROOT_DIR="$HOME"' >> ~/.bashrc
     echo "" >> ~/.zshrc
     echo "# The Commbase directory for the Z shell" >> ~/.zshrc
-    echo 'export COMMBASE="$HOME/Developer/mydroidandi"' >> ~/.zshrc
+    echo 'export COMMBASE_ROOT_DIR="$HOME"' >> ~/.zshrc
 fi
 
 exit 99
