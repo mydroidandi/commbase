0dfcae77971b9b9e76c2baca23039dc2e27443dc
Esteban Herrera <stv.herrera@gmail.com>
2023-04-19 18:31:29 -0500

    Fix the indentation to tab width: 2

--- config-scripts/set-commbase-root-directory.sh
+++ config-scripts/set-commbase-root-directory.sh
@@ -1,15 +1,15 @@
 #!/bin/env bash
 # set-commbase-root-directory.sh
 
-# Set environment variable permanently, making each user to have a
-# separate setting
+# Set environment variable permanently, making each user to have a separate 
+# setting
 if [[ ! -d "$COMMBASE_ROOT_DIR" ]]; then 
-    echo "" >> ~/.bashrc
-    echo "# The Commbase directory for the Bash shell" >> ~/.bashrc
-    echo 'export COMMBASE_ROOT_DIR="$HOME"' >> ~/.bashrc
-    echo "" >> ~/.zshrc
-    echo "# The Commbase directory for the Z shell" >> ~/.zshrc
-    echo 'export COMMBASE_ROOT_DIR="$HOME"' >> ~/.zshrc
+	echo "" >> ~/.bashrc
+	echo "# The Commbase directory for the Bash shell" >> ~/.bashrc
+	echo 'export COMMBASE_ROOT_DIR="$HOME"' >> ~/.bashrc
+	echo "" >> ~/.zshrc
+	echo "# The Commbase directory for the Z shell" >> ~/.zshrc
+	echo 'export COMMBASE_ROOT_DIR="$HOME"' >> ~/.zshrc
 fi
 
 exit 99
