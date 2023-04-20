389fd78da1c2a23798dcc308ee1f3470e470bb90
Esteban Herrera <stv.herrera@gmail.com>
2023-04-19 19:34:17 -0500

    Improve header and code comments

--- config-scripts/set-commbase-root-directory.sh
+++ config-scripts/set-commbase-root-directory.sh
@@ -1,8 +1,9 @@
 #!/bin/env bash
 # set-commbase-root-directory.sh
+# Sets the environment variable permanently, making each user to have a separate
+# setting.
 
-# Set environment variable permanently, making each user to have a separate 
-# setting
+# Check whether the directory specified by the variable does not exist
 if [[ ! -d "$COMMBASE_ROOT_DIR" ]]; then 
 	echo "" >> ~/.bashrc
 	echo "# The Commbase directory for the Bash shell" >> ~/.bashrc
