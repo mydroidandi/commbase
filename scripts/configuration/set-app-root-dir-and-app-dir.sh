30d34d9a592bd1e236ae3c7b2f469db62c22e709
Esteban Herrera <stv.herrera@gmail.com>
2023-05-05 17:14:43 -0500

    Add file headers

--- scripts/configuration/set-app-root-dir-and-app-dir.sh
+++ scripts/configuration/set-app-root-dir-and-app-dir.sh
@@ -1,5 +1,36 @@
 #!/bin/env bash
-# set-app-root-dir-and-app-dir.sh.
+################################################################################
+#                                   Commbase                                   #
+#                                                                              #
+# Conversational AI Assistant and AI Hub for Computers and Droids              #
+#                                                                              #
+# Change History                                                               #
+# 04/29/2023  Esteban Herrera Original code.                                   #
+#                           Add new history entries as needed.                 #
+#                                                                              #
+#                                                                              #
+################################################################################
+################################################################################
+################################################################################
+#                                                                              #
+#  Copyright (c) 2022-present Esteban Herrera C.                               #
+#  stv.herrera@gmail.com                                                       #
+#                                                                              #
+#  This program is free software; you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation; either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program; if not, write to the Free Software                 #
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+
+# set-app-root-dir-and-app-dir.sh
 # Prompts the user to enter the name of a new Commbase App directory and 
 # optionally the path of the App root directory. If the user enters an empty
 # string for the App directory name, it will keep asking until a non-empty 
@@ -9,7 +40,7 @@
 # exists. If the directory does not exist, the script exports the path to the
 # directory to the Bash shell's and the Z shell's configuration files (.bashrc
 # and .zshrc). Finally, the script exits with a status code of 99.
-# Remove the  lines from the files ~/.bashrc and ~/.zshrc before rerun this 
+# Remove the lines from the files ~/.bashrc and ~/.zshrc before rerun this 
 # setup.
 
 read -p "Enter the name of the App directory: " APP
