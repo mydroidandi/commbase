631948b6aacc1b22e657232319bb7d1b3137b8a8
Esteban Herrera <stv.herrera@gmail.com>
2023-05-06 17:48:36 -0500

    Add headers to scripts, rename file names, and update in the installation document

--- scripts/configuration/append-function-no-history-to-bashrc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# append-function-nohistory-to-bashrc.sh
-# Prevents a command from being saved in the command history by using the
-# HISTCONTROL environment variable.
-# The HISTCONTROL variable determines which commands are saved in the history
-# file. If you set it to the value "ignoreboth", any command preceded by a space
-# character will be excluded from the history.
-for line in '' '# Prevents a command from being saved in the command history by using the' '# HISTCONTROL environment variable.' 'no_history() { HISTCONTROL=ignoreboth; "$@"; unset HISTCONTROL; }'; do echo "$line" >> ~/.bashrc; done
