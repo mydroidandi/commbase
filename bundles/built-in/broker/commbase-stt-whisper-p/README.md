5635bc791548c7c02f14894cb8c9b3ea08e8af7a
Esteban Herrera <stv.herrera@gmail.com>
2023-06-09 01:10:05 -0500

    Add new whisper stt engine files

---
+++ bundles/built-in/broker/commbase-stt-whisper-p/README.md
@@ -0,0 +1,33 @@
+# commbase-stt-whisper-p
+
+[![Python Package using Conda](https://github.com/mydroidandi/commbase/actions/workflows/python-package-conda.yml/badge.svg)](https://github.com/mydroidandi/commbase/actions/workflows/python-package-conda.yml)
+[![python version](https://img.shields.io/badge/python-3.10%20%7C%203.11-blue)](https://img.shields.io/badge/python-3.10%20%7C%203.11-blue)
+
+## Introduction
+
+commbase-stt-whisper-p is an ASR (Automatic Speech Recognition) engine. It is capable of converting spoken language into written text without requiring an internet connection, making it a reliable and secure solution for any application that needs speech-to-text functionality.
+
+<img alt="commbase-stt-whisper-p" src="commbase-stt-whisper-p.jpg?raw=true" width="512" height="512" />
+
+## Controls
+
+It comes with 13 reliable mutually exclusive control messages that can be recorded and sent forward together with the parsed messages and be used by skills' applications to parameterize any skill option or be bypassed for message management/handling in any application subsequently. These control messages can also be changed or customized in their respective pattern files, and ultimately, increase or decrease in number, depending on your processing power and coding ability to remove or create new instances. These out-of-the-box controls are:
+
+* CONTROL_STOP_PREVIOUS_COMMAND
+* CONTROL_ACCEPT_CHANGES
+* CONTROL_DENY_CHANGES
+* CONTROL_SELECT_OPTION_NUMBER_ONE
+* CONTROL_SELECT_OPTION_NUMBER_TWO
+* CONTROL_SELECT_OPTION_NUMBER_THREE
+* CONTROL_SELECT_OPTION_NUMBER_FOUR
+* CONTROL_SKIP_QUESTION
+* CONTROL_REQUEST_THE_CURRENT_MODE
+* CONTROL_ENTER_THE_NORMAL_MODE
+* CONTROL_ENTER_THE_CONVERSATION_MODE
+* CONTROL_ENTER_THE_EXPERT_MODE
+* CONTROL_ENTER_THE_FOLLOW_MODE
+
+## Examples
+
+Detailed information about examples can be found in the corresponding [`examples`](./examples) directory.
+
---
+++ bundles/built-in/broker/commbase-stt-whisper-p/commbase_stt_whisper_p.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+################################################################################
+#                            commbase-stt-whisper-p                            #
+#                                                                              #
+# Conversational AI Assistant and AI Hub for Computers and Droids              #
+#                                                                              #
+# Change History                                                               #
+# 04/29/2023  Esteban Herrera Original code.                                   #
+#                           Add new history entries as needed.                 #
+#                                                                              #
+#                                                                              #
+################################################################################
+################################################################################
+################################################################################
+#                                                                              #
+#  Copyright (c) 2022-present Esteban Herrera C.                               #
+#  stv.herrera@gmail.com                                                       #
+#                                                                              #
+#  This program is free software; you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation; either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program; if not, write to the Free Software                 #
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+
+# commbase_stt_whisper_p.py
+# Records audio input from the microphone, performs speech recognition on the
+# recorded audio, and prints the recognized text.
+# Requires the Whisper model used in the code. The first execution of the
+# program downloads it automatically.
+# Requires to run redirecting the standard error output (stderr) to the null 
+# device (/dev/null) like this to avoid ALSA output messages:
+# shell> python commbase_stt_whisper_p.py 2>& /dev/null
+
+
+# Requirements
+import io
+from pydub import AudioSegment
+import speech_recognition as sr
+import whisper
+import tempfile
+import os
+import pyttsx3
+
+# A temporary directory and a file path within that directory
+temp_file = tempfile.mkdtemp()
+save_path = os.path.join(temp_file, 'temp.wav')
+
+# Initialize the text-to-speech engine, retrieve the available voices, and set
+# properties for the engine's rate and voice.
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('rate', 145)
+engine.setProperty('voice', voices[11].id)  #TODO: Add to the file INSTALL
+
+
+def listen():
+	"""
+	Initializes a speech recognizer, captures audio input from a microphone,
+	adjusts for ambient noise, saves the recorded audio as a WAV file, and handles
+	any exceptions that may occur during the process, and then returns the path of
+	the saved audio file.
+	"""
+	listener = sr.Recognizer()  # Create an instance of Recognizer
+	try:
+		with sr.Microphone() as source:
+			print("END USER:")
+			listener.adjust_for_ambient_noise(source)
+			audio = listener.listen(source)
+			data = io.BytesIO(audio.get_wav_data())
+			audio_clip = AudioSegment.from_file(data)
+			audio_clip.export(save_path, format='wav')
+	except Exception as e:
+		print(e)
+	return save_path
+	
+
+def recognize_audio(save_path):
+	"""
+	Uses the Whisper library to load a pre-trained audio recognition model and
+	transcribes an audio file located at save_path using that model. It then
+	returns the transcribed text extracted from the transcription result.
+	"""
+	audio_model = whisper.load_model('base')
+	transcription = audio_model.transcribe(save_path, language='english', fp16=False)
+	return transcription['text']
+
+
+def main():
+	"""
+	Servers as the entry point of the program and orchestrates the speech 
+	recognition process.
+	"""
+	response = recognize_audio(listen())
+	print(response).strip()
+
+
+# Ensure that the main() function is executed only when the script is run
+# directly as the main program.
+if __name__ == '__main__':
+	main()
