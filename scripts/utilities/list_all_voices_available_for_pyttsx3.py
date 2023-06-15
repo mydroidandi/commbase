0c22ca64f357db03a696022bdefb6b7c20b7f881
Esteban Herrera <stv.herrera@gmail.com>
2023-06-15 17:45:26 -0500

    Add new file list_all_voices_available_for_pyttsx3.py

---
+++ scripts/utilities/list_all_voices_available_for_pyttsx3.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+################################################################################
+#                                   Commbase                                   #
+#                                                                              #
+# Conversational AI Assistant and AI Hub for Computers and Droids              #
+#                                                                              #
+# Change History                                                               #
+# 04/29/2023  Esteban Herrera Original code.                                   #
+#                           Add new history entries as needed.                 #
+#                                                                              #
+#                                                                              #
+################################################################################
+################################################################################
+################################################################################
+#                                                                              #
+#  Copyright (c) 2022-present Esteban Herrera C.                               #
+#  stv.herrera@gmail.com                                                       #
+#                                                                              #
+#  This program is free software; you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation; either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program; if not, write to the Free Software                 #
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+
+# reset_commbase.conf.sh
+# Get and then print a list of voices available for pyttsx3
+
+# Requirements
+import pyttsx3
+
+engine = pyttsx3.init()
+
+# Print all available voices
+voices = engine.getProperty('voices')
+for voice in voices:
+	print("Voice:")
+	print(" - ID: %s" % voice.id)
+	print(" - Name: %s" % voice.name)
+	print(" - Languages: %s" % voice.languages)
+	print(" - Gender: %s" % voice.gender)
+	print(" - Age: %s" % voice.age)
+	
