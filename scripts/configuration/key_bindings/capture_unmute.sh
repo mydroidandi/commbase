86864e8f2467108fd1496a51cdc8bdbb172fce6d
Esteban Herrera <stv.herrera@gmail.com>
2023-10-11 16:02:04 -0500

    Update high quality audio devices keybinding files

--- scripts/configuration/key_bindings/capture_unmute.sh
+++ scripts/configuration/key_bindings/capture_unmute.sh
@@ -31,21 +31,8 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
 
 # capture_unmute.sh
-# A key binding, or an association between a physical key on a keyboard and a parameter
-# to unmute the sound capture.
-
-# Set up this file using your favorite Desktop Environment or Window Manager key binding
-# configuration.
-# For example, in Cinnamon, a Gnome based Desktop Environment, the keybinding steps are:
-# Go to Menu -> System Settings -> Keyboard -> shortcuts -> Categories -> Sound and Media ->
-# Add custom shortcut. 
-# Next, give the new shortcut a new name like "capture un-mute".
-# Next, select the route to this file.
-# Next, click or tap on the button Add the keyboard shortcut. The shortcut appears in the list.
-# Next, click on unassigned to pick an accelerator.
-# Next, press the keys ALT + SHIFT + 3 (all together) and then release them at a time.
-# Then the new key binding appears on the list.
-# Finally, verify that the key binding works.
+# A key binding, or an association between a physical key on a keyboard and a
+# parameter to unmute the sound capture.
 
 # Starts capturing sound that the Commbase recognition requires to work.
 # Uses the keyboard binding ALT-SHIFT-3.
@@ -56,4 +43,3 @@ capture_unmute() {
 capture_unmute || exit 99;
 
 exit 99
-
