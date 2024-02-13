9342a40fe30e75bd6e0a61e2404446095296a5d8
Esteban Herrera <stv.herrera@gmail.com>
2024-02-13 01:37:01 -0600

    Updater instead of uploader

--- bundles/built-in/broker/commbase-data-exchange/client/updater_http_websocket.py
+++ bundles/built-in/broker/commbase-data-exchange/client/updater_http_websocket.py
@@ -30,7 +30,7 @@
 #  along with this program; if not, write to the Free Software                 #
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
 
-# uploader_http_websocket.py
+# updater_http_websocket.py
 # Send a PUT request to update JSON data on a server
 
 import json
